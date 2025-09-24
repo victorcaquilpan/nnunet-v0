a51dbea8c626e1624cbdd9e49ccb24ab7d2d0e4f
root <victor.caquilpan@gmail.com>
2025-09-24 02:39:36 +0000

    changing return in simpleitk

--- nnunetv2/imageio/simpleitk_reader_writer.py
+++ nnunetv2/imageio/simpleitk_reader_writer.py
@@ -109,7 +109,8 @@ class SimpleITKIO(BaseReaderWriter):
             # are returned x,y,z but spacing is returned z,y,x. Duh.
             'spacing': spacings_for_nnunet[0]
         }
-        return np.vstack(images, dtype=np.float32, casting='unsafe'), dict
+        #return np.vstack(images, dtype=np.float32, casting='unsafe'), dict
+        return np.vstack(images).astype(np.float32), dict
 
     def read_seg(self, seg_fname: str) -> Tuple[np.ndarray, dict]:
         return self.read_images((seg_fname, ))
