72997dea0e1032da1ba99acf701f2cdba57a22c2
root <victor.caquilpan@gmail.com>
2025-09-29 05:24:34 +0000

    uploading pred and evaluation

--- nnunetv2/evaluation/evaluate_predictions.py
+++ nnunetv2/evaluation/evaluate_predictions.py
@@ -254,9 +254,9 @@ if __name__ == '__main__':
 
     #evaluate_folder_entry_point()
 
-    folder_ref = '/data/nnunet-environment/nnUNet_raw/Dataset004_BrainTissueRaw1000/labelsTs/'
-    folder_pred = '/data/nnunet-environment/nnUNet_results/Dataset004_BrainTissueRaw1000/nnUNetTrainer__nnUNetPlans__3d_fullres/predictions/'
-    output_file = '/data/nnunet-environment/nnUNet_results/Dataset004_BrainTissueRaw1000/nnUNetTrainer__nnUNetPlans__3d_fullres/summary_pred.json'
+    folder_ref = '/data/nnunet-environment/nnUNet_raw/Dataset003_BrainTissueRaw4Mod/labelsTs/'
+    folder_pred = '/data/nnunet-environment/nnUNet_results/Dataset003_BrainTissueRaw4Mod/nnUNetTrainer__nnUNetPlans__3d_fullres/predictions/'
+    output_file = '/data/nnunet-environment/nnUNet_results/Dataset003_BrainTissueRaw4Mod/nnUNetTrainer__nnUNetPlans__3d_fullres/summary_pred.json'
     image_reader_writer = SimpleITKIO()
     file_ending = '.nii.gz'
     regions = labels_to_list_of_regions(list(range(1, 30)))
